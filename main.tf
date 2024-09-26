c8947744089f804a9e1d859adfeb7e541aa15434
dev <dev@local.local>
2024-09-26 21:48:09 +0700

    Added code for determine AWS Account ID

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Get the AWS account ID
+data "aws_caller_identity" "current" {}
+
 variable "db_password" {
   type        = string
   description = "Database password"
@@ -20,4 +23,10 @@ module "get_secret_lambda" {
   source      = "./modules/get-secret"
   secret_arn  = module.db_secret.secret_arn
   secret_name = "rds/postgresql/db_password"
+}
+
+# Output the AWS account ID
+output "account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "The AWS Account ID"
 }
